5feb681b46c9639154c7dc06f04cb566e5494771
PavelKhmyz <pavlik.schavlik@outlook.com>
2023-02-06 20:01:35 +0300

    keep styltng

--- src/components/home/components/reactCarouselResponsive.ts
+++ src/components/home/components/reactCarouselResponsive.ts
@@ -12,15 +12,15 @@ export const responsive = {
     partialVisibilityGutter: -1,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 520 },
     items: 2,
     slidesToSlide: 2,
     partialVisibilityGutter: -1,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 520, min: 0 },
     items: 1,
-    slidesToSlide: 1,
+    slidesToSlide: 0,
     partialVisibilityGutter: -1,
   },
 };
